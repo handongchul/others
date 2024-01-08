f0b057e1a25caa247cbde3284e166d2f020fe531
handongchul <hansco75495@gmail.com>
2024-01-08 11:31:36 +0900

    ALL

---
+++ toy_fastapis_t/databases/connections.py
@@ -0,0 +1,60 @@
+from typing import Any, List, Optional
+
+from beanie import init_beanie, PydanticObjectId
+from models.users import User
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
+# 변경 후 코드
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: Optional[str] = None
+    
+    async def initialize_database(self):
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+        await init_beanie(database=client.get_default_database(),
+                          document_models=[User])
+
+    class Config:
+        env_file = ".env"
+
+class Database:
+    # model 즉 collection
+    def __init__(self, model) -> None:
+        self.model = model
+        pass       
+
+    # 전체 리스트
+    async def get_all(self) :
+        documents = await self.model.find_all().to_list()   # find({})
+        pass
+        return documents
+    
+    # 상세 보기
+    async def get(self, id: PydanticObjectId) -> Any:
+        doc = await self.model.get(id)  # find_one()
+        if doc:
+            return doc
+        return False    
+    
+    # 저장
+    async def save(self, document) -> None:
+        await document.create()
+        return None   
+     
+    # column 값으로 여러 Documents 가져오기
+    async def getsbyconditions(self, conditions:dict) -> [Any]:
+        documents = await self.model.find(conditions).to_list()  # find({})                 #condition에 해당하는 값들을 가져와 리스트로 구성
+        if documents:
+            return documents
+        return False    
+    
+if __name__ == '__main__':
+    settings = Settings()
+    async def init_db():
+        await settings.initialize_database()
+
+    collection_user = Database(User)
+    conditions = "{ name: { $regex: '이' } }"
+    list = collection_user.getsbyconditions(conditions)
+    pass
